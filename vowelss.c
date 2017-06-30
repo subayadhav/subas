3ebffe017f564f72f3a37f5c7bcb5f85cd59ca8c
subayadhav <suba10499@gmail.com>
2017-06-30 11:12:45 +0530

    Rename vowels.c to vowelss.c

---
+++ vowelss.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s;
+clrscr();
+printf("\n enter the variable");
+scanf("%d",&s);
+if((s=='a')||(s=='e')||(s=='i')||(s=='o')||(s=='u'));
+printf("\n vowels");
+else
+{
+printf("\n consonant");
+}
+getch();
+}
