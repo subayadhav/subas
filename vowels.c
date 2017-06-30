ee9ce3dc541eb888a859dc001af10c21cbb92e56
subayadhav <suba10499@gmail.com>
2017-06-30 10:43:10 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s,a,e,i,o,u;
+clrscr();
+printf("\n enter the letter");
+scanf("%d",&s);
+if((s==a)&&(s==e)&&(s==i)&&(s==o)&&(s==u))
+printf("\n the letter is vowel");
+else
+printf("\n the letter is consonant");
+getch();
+}
